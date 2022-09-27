209510444bbc389a014dbf3a0165350675bf7b33
Woohyun Park <woohyunpark.mail@gmail.com>
2022-09-27 21:00:15 +0900

    PROCESS, finish q3

--- hw2.lisp
+++ hw2.lisp
@@ -4,7 +4,8 @@
         (print (format nil "Incorrect! Expected: ~a Got: ~a" b a))))
 
 (defun count-all (target lst)
-    (cond ((equal target lst) 1)
+    (cond
+        ((equal target lst) 1)
         ((or (not (listp lst)) (null lst)) 0)
         (t (+ (count-all target (car lst)) (count-all target (cdr lst))))))
 
@@ -16,11 +17,26 @@
 (terpri)
 
 (defun remove-all2 (target lst)
-;code here
-)
+    (cond
+        ((atom lst) lst)
+        ((equal target (car lst)) (remove-all2 target (cdr lst)))
+        (t (cons (remove-all2 target (car lst)) (remove-all2 target (cdr lst))))))
         
 (print "TEST: remove-all2")
 (my-assert (remove-all2 'a '(b (a n) a n a)) '(b (n) n))
 (my-assert (remove-all2 'a '((a b (c a))(b (a c) a))) '((b (c))(b (c))))
 (my-assert (remove-all2 '(a b) '(a (a b)((c (a b)) b))) '(a ((c) b)))
 (terpri)
+
+(defun reverse-all (lst)
+    (cond
+        ((consp lst) (append (reverse-all (cdr lst)) (list (reverse-all (car lst)))))
+        (t lst)))
+
+(print "TEST: reverse-all")
+(my-assert (reverse-all '((1 2) (3 4) 5)) '(5 (4 3) (2 1)))
+(my-assert (reverse-all '(a (b c)(d (e f)))) '(((f e) d)(c b) a))
+(terpri)
+
+(defun depth (lst)
+    )
